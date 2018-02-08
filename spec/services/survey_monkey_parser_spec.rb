a3c46a173ac45fdf9559ad9d2f841f085a3dc1fb
Natasha Montero <natashamontero@gmail.com>
2018-02-08 14:52:10 -0400

    add some specs

--- spec/services/survey_monkey_parser_spec.rb
+++ spec/services/survey_monkey_parser_spec.rb
@@ -15,25 +15,75 @@ describe SurveyMonkeyParser do
       expect(responses.size).to eq(2)
     end
 
-    context 'when use_weights is false' do
-      it "returns an average score from text attribute" do
-        expect(response.key?('score')).to be true
-        expect(response['score']).to eq(1)
+    context "when score_question_ids is not present" do
+      context 'when use_weights is false' do
+        it "returns an average score from text attribute" do
+          expect(response.key?('score')).to be true
+          expect(response['score']).to eq(1)
+        end
+      end
+
+      context 'when use_weights is true' do
+        let(:service) { described_class.new(data.merge('use_weights' => true)) }
+
+        it "returns an average score from weight attribute" do
+          expect(response.key?('score')).to be true
+          expect(response['score']).to eq(2)
+        end
       end
     end
 
-    context 'when use_weights is true' do
-      let(:service) { described_class.new(data.merge('use_weights' => true)) }
+    context 'when score_question_ids is present' do
+      let(:options) { {'score_question_ids' => '759363652,759363654'} }
+      let(:service) { described_class.new(data.merge(options)) }
 
-      it "returns an average score from weight attribute" do
+      it 'returns the score from the first question matched' do
         expect(response.key?('score')).to be true
-        expect(response['score']).to eq(2)
+        expect(response['score']).to eq(5)
+      end
+
+      context 'when use_weights is false' do
+        it "returns an average score from text attribute" do
+          expect(response.key?('score')).to be true
+          expect(response['score']).to eq(5)
+        end
+      end
+
+      context 'when use_weights is true' do
+        let(:options) { {'use_weights' => true, 'score_question_ids' => '759363652,759363654'} }
+
+        it "returns an average score from weight attribute" do
+          expect(response.key?('score')).to be true
+          expect(response['score']).to eq(5)
+        end
       end
     end
 
-    it "returns a comment attribute" do
-      expect(response.key?('comment')).to be true
-      expect(response['comment']).to eq("Dreadful customer service")
+    context 'when comment_question_ids is not present' do
+      it 'returns the comment from the first question matched' do
+        expect(response.key?('comment')).to be true
+        expect(response['comment']).to eq("Dreadful customer service")
+      end
+    end
+
+    context 'when comment_question_ids is present' do
+      let(:options) { {'comment_question_ids' => '759363659'} }
+      let(:service) { described_class.new(data.merge(options)) }
+
+      it 'returns the comment from the first question matched' do
+        expect(response.key?('comment')).to be true
+        expect(response['comment']).to eq("Get new staff that are not useless ")
+      end
+    end
+
+    context 'when comment is multi answers' do
+      let(:options) { {'comment_question_ids' => '759363657'} }
+      let(:service) { described_class.new(data.merge(options)) }
+
+      it 'returns the comments joined with \n char' do
+        expect(response.key?('comment')).to be true
+        expect(response['comment']).to eq("me@foo.com\n12345")
+      end
     end
 
     it "returns the response id attribute" do
@@ -75,60 +125,70 @@ describe SurveyMonkeyParser do
     end
 
     it "returns a full_response attribute with all response answers" do
-      expected = {
-        '759363655' => {
-          id: '759363655',
-          family: 'single_choice',
-          subtype: 'vertical',
-          question: 'Did we answer your question?',
-          answers: {
-            'Did we answer your question?' => 'No'
-          }
-        },
-        '759363654' => {
-          id: '759363654',
-          family: 'matrix',
-          subtype: 'rating',
-          question: 'In the following area, how would you evaluate the agent who answered you?',
-          answers: {
-            'Enthusiasm' => 'Very bad 0',
-            'Professionalism' => 'Very bad 0',
-            'Understanding of your needs' => 'Good 3',
-            'Clarity of answer' => 'Very bad 0',
-            'Patience' => 'Very bad 0'
-          }
-        },
-        '759363658' => {
-          id: '759363658',
-          family: 'open_ended',
-          subtype: 'essay',
-          question: 'How could we improve the service?',
-          answers: {
-            'How could we improve the service?' => 'Dreadful customer service'
-          }
-        },
-        '759363659' => {
-          id: '759363659',
-          family: 'open_ended',
-          subtype: 'essay',
-          question: 'What would be your suggestions to improve Vivastreet ?',
-          answers: {
-            'What would be your suggestions to improve Vivastreet ?' => 'Get new staff that are not useless '
-          }
-        },
-        '759363657' => {
-          id: '759363657',
-          family: 'open_ended',
-          subtype: 'multi',
-          question: 'If you wish, enter your contact information below so we can contact you if necessary:',
-          answers: {
-            'Email address' => 'me@foo.com',
-            'Telephone number' => '12345'
-          }
-        },
-      }
-      expect(response.key?('full_response')).to be true
-      expect(response['full_response']).to eq(expected)
-    end
+    expected = {
+      '759363655' => {
+        id: '759363655',
+        family: 'single_choice',
+        subtype: 'vertical',
+        question: 'Did we answer your question?',
+        answers: {
+          'Did we answer your question?' => 'No'
+        }
+      },
+      '759363654' => {
+        id: '759363654',
+        family: 'matrix',
+        subtype: 'rating',
+        question: 'In the following area, how would you evaluate the agent who answered you?',
+        answers: {
+          'Enthusiasm' => 'Very bad 0',
+          'Professionalism' => 'Very bad 0',
+          'Understanding of your needs' => 'Good 3',
+          'Clarity of answer' => 'Very bad 0',
+          'Patience' => 'Very bad 0'
+        }
+      },
+      '759363652' => {
+        id: '759363652',
+        family: 'single_choice',
+        subtype: 'vertical',
+        question: 'In general, what do you think of the answer you have received?',
+        answers: {
+          'In general, what do you think of the answer you have received?' => 'Excellent 5'
+        }
+      },
+      '759363658' => {
+        id: '759363658',
+        family: 'open_ended',
+        subtype: 'essay',
+        question: 'How could we improve the service?',
+        answers: {
+          'How could we improve the service?' => 'Dreadful customer service'
+        }
+      },
+      '759363659' => {
+        id: '759363659',
+        family: 'open_ended',
+        subtype: 'essay',
+        question: 'What would be your suggestions to improve Vivastreet ?',
+        answers: {
+          'What would be your suggestions to improve Vivastreet ?' => 'Get new staff that are not useless '
+        }
+      },
+      '759363657' => {
+        id: '759363657',
+        family: 'open_ended',
+        subtype: 'multi',
+        question: 'If you wish, enter your contact information below so we can contact you if necessary:',
+        answers: {
+          'Email address' => 'me@foo.com',
+          'Telephone number' => '12345'
+        }
+      },
+    }
+    expect(response.key?('full_response')).to be true
+    expect(response['full_response']).to eq(expected)
+  end
+
   end
 end
