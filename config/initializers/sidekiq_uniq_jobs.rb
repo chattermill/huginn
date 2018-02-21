f7048244324d326a6e13e05581b5854b7505e0ed
Natasha Montero <natashamontero@gmail.com>
2018-02-21 16:08:03 -0400

    fix and specs

--- config/initializers/sidekiq_uniq_jobs.rb
+++ config/initializers/sidekiq_uniq_jobs.rb
@@ -5,13 +5,13 @@ require 'sidekiq-unique-jobs'
 Sidekiq.default_worker_options = {
   unique: :until_executed,
   unique_across_workers: true,
-  log_duplicate_payload: true,
   unique_args: lambda { |args|
-    r = args.first.except('job_id')
-    # this ensure to be unique by adding the 'make_unique' argument
-    # to all agents other than AgentCheckJob
-    r[:make_unique] = SecureRandom.uuid unless r['job_class'] == 'AgentCheckJob'
-    [r]
+    if args.first['job_class'] == 'AgentCheckJob'
+      [args.first.except('job_id')]
+    else
+      [args.first]
+    end
+
   }
 }
 SidekiqUniqueJobs.config.unique_args_enabled = true
