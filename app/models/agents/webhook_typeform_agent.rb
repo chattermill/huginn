bf5e0f56e679c97d17c5430306c229c4d97538bd
Natasha <natashamontero@gmail.com>
2018-03-12 18:44:57 -0400

    Add Typeform Webhook agent (#95)
    * add typeform webhook response
    
    * adding typeform response payload
    
    * fix specs
    
    * update webhook only if options changed
    
    * Configure Circle CI test machines and Rails to use the same Timezone
    
    * fix timezone'
    
    * fix specs
    
    * fix ambiguos match name for agents (specs) and reverse circle timezone.

---
+++ app/models/agents/webhook_typeform_agent.rb
@@ -0,0 +1,317 @@
+module Agents
+  class WebhookTypeformAgent < WebhookAgent
+    include FormConfigurable
+
+    TYPEFORM_URL_BASE = 'https://api.typeform.com'.freeze
+
+    cannot_be_scheduled!
+    cannot_receive_events!
+
+    before_validation :parse_json_options
+    after_save :set_typeform_webhook
+
+    description do
+      <<-MD
+        The webhook agent for typeform will create events by receiving webhooks from Typeform.
+        In order to create events with this agent, make a POST request to:
+
+        ```
+           https://#{ENV['DOMAIN']}/users/#{user.id}/web_requests/#{id || ':id'}/#{options['secret'] || ':secret'}
+        ```
+
+        #{'The placeholder symbols above will be replaced by their values once the agent is saved.' unless id}
+
+        Options:
+
+          * `access_token` - Typeform API Key.
+          * `form_id` - Typeform Form ID.
+          * `guess_mode` - Let the agent try to figure out the score question and the comment question automatically using the first `opinionscale` question and the first `textarea` question
+          * `score_question_ids` - Hard-code the comma separated list of ids of the score questions (agent will pick the first one present) if `guess_mode` is off. Only Id, example: `"58048493,58048049"`
+          * `comment_question_ids` - Hard-code he comma separated list of ids of the comment questions (agent will pick the first one present) if `guess_mode` is off. Only Id, example: `"5804976588,58049969,58049765"`
+          * `mapping_object` - Specify the mapping definition object where any hidden_variables can be mapped with a single value.
+          * `bucketing_object` - Specify the bucketing definition object where any hidden_variables can be broken into a specific bucket.
+          * `expected_receive_period_in_days` - How often you expect to receive events this way. Used to determine if the agent is working.
+      MD
+    end
+
+
+    event_description do
+      <<-MD
+        Events look like this:
+
+        "score": 9,
+        "comment": "Love the concept and the food! Just a little too expensive.",
+        "id": "62e3caeaca5100adf84f61708ad69960",
+        "created_at": "2017-08-12 19:16:02",
+        "answers": [
+          {
+            "field": {
+              "id": "58048493",
+              "type": "opinion_scale"
+            },
+            "type": "number",
+            "number": 4
+          },
+          {
+            "field": {
+              "id": "58049765",
+              "type": "dropdown"
+            },
+            "type": "text",
+            "text": "£30,000 - £39,999"
+          },
+          {
+            "field": {
+              "id": "58049969",
+              "type": "long_text"
+            },
+            "type": "text",
+            "text": "I do like knowing about my financial state and the Clearscore report  makes me feel like I'm doing something right. However I don't think my world would end if I did'nt have Clearscore, but I do look forward to their next update  "
+          },
+          {
+            "field": {
+              "id": "58049393",
+              "type": "multiple_choice"
+            },
+            "type": "choice",
+            "choice": {
+              "label": "55 to 64"
+            }
+          }
+        ],
+        "formatted_answers": {
+          "opinion_scale_58048690": 3,
+          "long_text_58049969": "It is really helpful to see your credit score at any time and if it changes then why gas it changed",
+          "opinion_scale_58048493": 5,
+          "opinion_scale_58048704": 2,
+          "dropdown_58049487": "South East",
+          "opinion_scale_58048049": 10,
+          "opinion_scale_58048684": 3,
+          "opinion_scale_58048687": 3,
+          "multiple_choice_58049393": {
+            "label": "55 to 64"
+          },
+          "dropdown_58049765": "< £20,000"
+        },
+        "metadata": {
+          "user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.0 Mobile/14G60 Safari/602.1",
+          "platform": "mobile",
+          "referer": "https://typeform.com/to/XXXX",
+          "network_id": "8f548786ca",
+          "browser": "touch"
+        },
+        "hidden_variables": {
+          "customer": "11223",
+          "name": "Susan",
+          "score": "2",
+          "survey_answer": "Somewhat+disappointed",
+          "survey_name": "login_monthly",
+          "city_id": "458",
+          "country_id": "25",
+          "tc": "2"
+        },
+        "mapped_variables": {
+          "city_id": "London"
+          "country_id": "UK",
+        }
+      MD
+    end
+
+
+    def default_options
+      {
+        'access_token' => '{% credential TypeformAccessToken %}',
+        'guess_mode' => true,
+        'mapping_object' => '{}',
+        'bucketing_object' => '{}',
+        "secret" => SecureRandom.hex,
+        "expected_receive_period_in_days" => 1,
+        "payload_path" => "."
+      }
+    end
+
+    form_configurable :access_token
+    form_configurable :form_id
+    form_configurable :guess_mode, type: :boolean
+    form_configurable :score_question_ids
+    form_configurable :comment_question_ids
+    form_configurable :mapping_object, type: :json, ace: { mode: 'json' }
+    form_configurable :bucketing_object, type: :json, ace: { mode: 'json' }
+    form_configurable :expected_receive_period_in_days
+    form_configurable :secret
+    form_configurable :payload_path
+
+    def validate_options
+      %w[access_token form_id].each do |key|
+        errors.add(:base, "The '#{key}' option is required.") if options[key].blank?
+      end
+
+      super
+    end
+
+    def receive_web_request(params, method, format)
+      # check the secret
+      secret = params.delete('secret')
+      return ["Not Authorized", 401] unless secret == interpolated['secret']
+
+      # check the verbs
+      verbs = (interpolated['verbs'] || 'post').split(/,/).map { |x| x.strip.downcase }.select { |x| x.present? }
+      return ["Please use #{verbs.join('/').upcase} requests only", 401] unless verbs.include?(method)
+
+      # check the code
+      code = (interpolated['code'].presence || 201).to_i
+
+      [payload_for(params)].flatten.each do |payload|
+        event_payload = transform_typeform_response(payload)
+        create_event(payload: event_payload) if event_payload
+      end
+
+      if interpolated['response_headers'].presence
+        [interpolated(params)['response'] || 'Event Created', code, "text/plain", interpolated['response_headers'].presence]
+      else
+        [interpolated(params)['response'] || 'Event Created', code]
+      end
+    end
+
+    private
+
+    def transform_typeform_response(payload)
+      response = payload['form_response']
+      return if response.blank?
+
+      answers = sorted_answers(response['answers'])
+      {
+        score: score_from_response(answers),
+        comment: comment_from_response(answers),
+        created_at: response['submitted_at'],
+        id: response['token'],
+        answers: answers,
+        formatted_answers: transform_answers(answers),
+        metadata: response['metadata'],
+        hidden_variables: response['hidden'],
+        mapped_variables: mapping_from_response(response)
+      }
+    end
+
+    def sorted_answers(answers)
+      (answers || []).sort_by { |el| el['field']['id'] }
+    end
+
+    def score_from_response(answers)
+      answer = if boolify(interpolated['guess_mode'])
+                 answers.find {|h| h['field']['type'] == "opinion_scale" }
+               else
+                 answer_for(answers, interpolated['score_question_ids'])
+               end
+
+      answer['number'] if answer.present?
+    end
+
+    def comment_from_response(answers)
+      answer = if boolify(interpolated['guess_mode'])
+                 answers.find { |h| h["field"]["type"] == "long_text" }
+               else
+                 answer_for(answers, interpolated['comment_question_ids'])
+               end
+
+      answer['text'] if answer.present?
+    end
+
+    def answer_for(answers, option_ids)
+      answers_ids = answers.map {|a| a['field']['id'] }
+      key = option_ids.split(',').find { |id| answers_ids.include?(id) }
+      answers.find {|h| h['field']['id'] == key }
+    end
+
+    def transform_answers(answers)
+      answers.each_with_object({}) do |a, hash|
+        hash["#{a['field']['type']}_#{a['field']['id']}"] = a.fetch(a['type'])
+      end
+    end
+
+    def mapping_from_response(response)
+      return if response['hidden'].blank?
+
+      mapped_variables = {}
+      mapped_variables.merge!(single_values_mapping(response))
+      mapped_variables.merge!(value_ranges_mapping(response))
+    end
+
+    def single_values_mapping(response)
+      hash = {}
+      mapping = options["mapping_object"]
+      if mapping.present?
+        response['hidden'].each do |k, v|
+          hash[k] = mapping[k].fetch(v, v) if mapping.key?(k)
+        end
+      end
+      hash
+    end
+
+    def value_ranges_mapping(response)
+      hash = {}
+      mapping = options["bucketing_object"]
+      if mapping.present?
+        response['hidden'].each do |k, v|
+          hash[k] = extract_bucket(mapping[k], v) || v if mapping.key?(k)
+        end
+      end
+      hash
+    end
+
+    def extract_bucket(hash, value)
+      value = value.to_i
+      bucket = nil
+      hash.each do |k, v|
+        range = k.split("-")
+        min = range.first
+        max = range.last
+        if /\+/ =~ max && value >= max.tr("+", "").to_i
+          bucket = v
+          break
+        elsif (min.to_i..max.to_i).cover?(value)
+          bucket = v
+          break
+        end
+      end
+      bucket
+    end
+
+    def parse_json_options
+      parse_json_option('mapping_object')
+      parse_json_option('bucketing_object')
+    end
+
+    def parse_json_option(key)
+      options[key] = JSON.parse(options[key]) unless options[key].is_a?(Hash)
+    rescue
+      errors.add(:base, "The '#{key}' option is an invalid JSON.")
+    end
+
+    def set_typeform_webhook
+      return unless saved_change_to_disabled? || saved_change_to_options?
+
+      enabled = !self.disabled
+      body = {
+        "url": "https://#{ENV['DOMAIN']}/users/#{user.id}/web_requests/#{id}/#{options['secret']}",
+        "enabled": enabled
+      }
+      response = faraday.put(webhook_url, body.to_json, headers(auth_header))
+      error "Failed: #{response.status}: #{response.body}" unless response.success?
+    end
+
+    def webhook_url
+      form = interpolated['form_id']
+      tag = "agent_#{id}"
+      "#{TYPEFORM_URL_BASE}/forms/#{form}/webhooks/#{tag}"
+    end
+
+    def auth_header
+      {
+        'Content-Type' => 'application/json; charset=utf-8',
+        'Authorization' =>  "Bearer #{interpolated['access_token']}"
+      }
+    end
+
+  end
+end
---
+++ spec/models/agents/webhook_typeform_agent_spec.rb
@@ -0,0 +1,357 @@
+require 'rails_helper'
+
+describe Agents::WebhookTypeformAgent do
+  before do
+    VCR.insert_cassette 'typeform_webhook', record: :none, :match_requests_on => [:host], :allow_playback_repeats => true
+
+    @opts = {
+      'access_token' => 'token123',
+      "form_id" => "jOyEkB",
+      'guess_mode' => true,
+      'mapping_object' => '{}',
+      'bucketing_object' => '{}',
+      "secret" => 'foobar',
+      "expected_receive_period_in_days" => 1,
+      "payload_path" => "."
+    }
+
+    @agent = Agents::WebhookTypeformAgent.new(:name => 'My agent', :options => @opts)
+    @agent.user = users(:bob)
+    @agent.save!
+  end
+
+  after do
+    VCR.eject_cassette
+  end
+
+  let(:payload) { JSON.parse(File.read(Rails.root.join("spec/data_fixtures/typeform/webhook.json"))) }
+
+  describe 'descriptions' do
+    it 'has agent description' do
+      expect(@agent.description).to_not be_nil
+    end
+
+    it 'renders the description markdown without errors' do
+      expect { @agent.description }.not_to raise_error
+    end
+
+    it 'has event description' do
+      expect(@agent.event_description).to_not be_nil
+    end
+  end
+
+  describe '#working' do
+    it 'is not working without having emitted an event' do
+      expect(@agent).not_to be_working
+    end
+
+    it 'it is working when at least one event was emitted' do
+      @event = Event.new
+      @event.agent = @agent
+      @event.payload = {
+        'comment' => 'somevalue'
+      }
+      @event.save!
+
+      expect(@agent.reload).to be_working
+    end
+
+    it 'is not working when there is a recent error' do
+      @agent.error 'oh no!'
+      expect(@agent.reload).not_to be_working
+    end
+  end
+
+  it "should have an empty schedule" do
+    expect(@agent.schedule).to be_nil
+    expect(@agent).to be_valid
+    @agent.schedule = "5pm"
+    @agent.save!
+    expect(@agent.schedule).to be_nil
+
+    expect(@agent.can_be_scheduled?).to eq false
+  end
+
+  it 'cannot receive events' do
+    expect(@agent.cannot_receive_events?).to eq true
+  end
+
+  describe 'validations' do
+    before do
+      expect(@agent).to be_valid
+    end
+
+    it 'should validate presence of access_token' do
+      @agent.options['access_token'] = ''
+      expect(@agent).not_to be_valid
+    end
+
+    it 'should validate presence of form_id' do
+      @agent.options['form_id'] = ''
+      expect(@agent).not_to be_valid
+    end
+  end
+
+  describe 'callback' do
+    it 'perform a request to create or update the typeform webhook' do
+      expect(WebMock).to have_requested(:put ,"https://api.typeform.com/forms/jOyEkB/webhooks/agent_#{@agent.id}")
+    end
+
+    it 'does not create or update the webhook if disabled or options have not changed' do
+      @agent.name = "Another name"
+      @agent.save
+      expect(WebMock).to have_requested(:put ,"https://api.typeform.com/forms/jOyEkB/webhooks/agent_#{@agent.id}").once
+    end
+
+    it 'log failed response' do
+      VCR.eject_cassette
+      VCR.use_cassette 'typeform_webhook_failed' do
+
+        @agent.options['access_token'] = '123'
+        @agent.save!
+
+        expect(@agent.logs.count).to eq(1)
+        expect(@agent.logs.last.message).to match(/Failed/)
+
+      end
+    end
+  end
+
+  describe 'receive_web_request' do
+    it 'should create event if secret matches' do
+      out = nil
+      expect {
+        out = @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+      }.to change { Event.count }.by(1)
+      expect(out).to eq(['Event Created', 201])
+      expect(Event.last.payload.key?('score')).to be true
+    end
+
+    it 'should not create event if secrets do not match' do
+      out = nil
+      expect {
+        out = @agent.receive_web_request(payload.merge('secret' => 'booo'), "post", "text/html")
+      }.to change { Event.count }.by(0)
+      expect(out).to eq(['Not Authorized', 401])
+    end
+
+    it 'should respond with `201`' do
+      out = @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+      expect(out).to eq(['Event Created', 201])
+    end
+
+    it "should accept POST" do
+      out = nil
+      expect {
+        out = @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+      }.to change { Event.count }.by(1)
+      expect(out).to eq(['Event Created', 201])
+    end
+
+    context 'when guess_mode is true' do
+      it 'emits correct payload' do
+        expected = {
+          'score' => 2,
+          'comment' => 'It\'s cold right now!',
+          'created_at' => '2018-01-18T18:17:02Z',
+          'id' => 'a3a12ec67a1365927098a606107fac15',
+          'answers' => [
+              {
+                  "type" => "text",
+                  "text" => "It's cold right now!",
+                  "field" => {
+                      "id" => "DlXFaesGBpoF",
+                      "type" => "long_text"
+                  }
+              },
+              {
+                  "type" => "text",
+                  "text" => "Laura",
+                  "field" => {
+                      "id" => "JwWggjAKtOkA",
+                      "type" => "short_text"
+                  }
+              },
+              {
+                  "type" => "number",
+                  "number" => 2,
+                  "field" => {
+                      "id" => "NRsxU591jIW9",
+                      "type" => "opinion_scale"
+                  }
+              },
+              {
+                  "type" => "number",
+                  "number" => 3,
+                  "field" => {
+                      "id" => "WOTdC00F8A3h",
+                      "type" => "rating"
+                  }
+              }
+          ],
+          'formatted_answers' => {
+            'long_text_DlXFaesGBpoF' => "It's cold right now!",
+            'short_text_JwWggjAKtOkA' => 'Laura',
+            'opinion_scale_NRsxU591jIW9' => 2,
+            'rating_WOTdC00F8A3h' => 3
+          },
+          'metadata' => nil,
+          'hidden_variables' => nil,
+          'mapped_variables' => nil
+        }
+        out = nil
+        expect {
+          out = @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+        }.to change { Event.count }.by(1)
+        expect(@agent.events.last.payload).to eq(expected)
+      end
+
+      it 'does not emit event if form_response field is not found' do
+        expect {
+          @agent.receive_web_request({'secret' => 'booo', 'some_key' => 'some value'}, "post", "text/html")
+        }.to change { Event.count }.by(0)
+      end
+
+      it 'emits empty answer if there is no answers' do
+        @agent.receive_web_request({'secret' => 'foobar', 'form_response' => { "answers" => [] }}, "post", "application/json")
+
+        expect(@agent.events.last.payload['answers']).to eq([])
+      end
+    end
+
+    context 'when guess_mode is true' do
+      before do
+        @agent.options['guess_mode'] = false
+        @agent.options['score_question_ids'] = 'WOTdC00F8A3h'
+        @agent.options['comment_question_ids'] = 'JwWggjAKtOkA'
+      end
+      it 'emits correct payload' do
+        expected = {
+          'score' => 3,
+          'comment' => 'Laura',
+          'created_at' => '2018-01-18T18:17:02Z',
+          'id' => 'a3a12ec67a1365927098a606107fac15',
+          'answers' => [
+              {
+                  "type" => "text",
+                  "text" => "It's cold right now!",
+                  "field" => {
+                      "id" => "DlXFaesGBpoF",
+                      "type" => "long_text"
+                  }
+              },
+              {
+                  "type" => "text",
+                  "text" => "Laura",
+                  "field" => {
+                      "id" => "JwWggjAKtOkA",
+                      "type" => "short_text"
+                  }
+              },
+              {
+                  "type" => "number",
+                  "number" => 2,
+                  "field" => {
+                      "id" => "NRsxU591jIW9",
+                      "type" => "opinion_scale"
+                  }
+              },
+              {
+                  "type" => "number",
+                  "number" => 3,
+                  "field" => {
+                      "id" => "WOTdC00F8A3h",
+                      "type" => "rating"
+                  }
+              }
+          ],
+          'formatted_answers' => {
+            'long_text_DlXFaesGBpoF' => "It's cold right now!",
+            'short_text_JwWggjAKtOkA' => 'Laura',
+            'opinion_scale_NRsxU591jIW9' => 2,
+            'rating_WOTdC00F8A3h' => 3
+          },
+          'metadata' => nil,
+          'hidden_variables' => nil,
+          'mapped_variables' => nil
+        }
+        out = nil
+        expect {
+          out = @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+        }.to change { Event.count }.by(1)
+        expect(@agent.events.last.payload).to eq(expected)
+      end
+
+      it 'does not emit event if form_response field is not found' do
+        expect {
+          @agent.receive_web_request({'secret' => 'booo', 'some_key' => 'some value'}, "post", "text/html")
+        }.to change { Event.count }.by(0)
+      end
+
+      it 'emits score as nil if does not find any score question' do
+        @agent.options['score_question_ids'] = 'abcde'
+        @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+
+        expect(@agent.events.last.payload['score']).to be nil
+      end
+
+      it 'emits comment as nil if does not find any comment question' do
+        @agent.options['comment_question_ids'] = 'abcde'
+        @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+
+        expect(@agent.events.last.payload['comment']).to be nil
+      end
+
+      it 'emits empty answer if there is no answers' do
+        @agent.receive_web_request({'secret' => 'foobar', 'form_response' => { "answers" => [] }}, "post", "application/json")
+
+        expect(@agent.events.last.payload['answers']).to eq([])
+      end
+    end
+
+    context 'with mapping_object' do
+      let(:payload) { JSON.parse(File.read(Rails.root.join("spec/data_fixtures/typeform/webhook_with_hidden_field.json"))) }
+
+      it 'build the mapped_variables correctly' do
+        @agent.options['mapping_object'] = {
+          "city_id": {
+            "0": "London",
+            "1": "New York"
+          },
+          "country_id": {
+            "1": "UK",
+            "8": "US"
+          }
+        }
+
+        expected = {
+          "city_id" => "New York",
+          "country_id" => "US"
+        }
+
+        @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+        expect(@agent.events.last.payload['mapped_variables']).to eq(expected)
+      end
+    end
+
+    context 'with bucketing_object' do
+      let(:payload) { JSON.parse(File.read(Rails.root.join("spec/data_fixtures/typeform/webhook_with_hidden_field.json"))) }
+
+      it 'build the mapped_variables correctly' do
+        @agent.options['bucketing_object'] = {
+          "tc": {
+            "1": "First Trip",
+            "2-10": "2 - 10",
+            "11-20": "11 - 20",
+            "21-50": "21 - 50",
+            "51-100": "51 - 100",
+            "100+": "101 and more"
+          }
+        }
+        @agent.receive_web_request(payload.merge('secret' => 'foobar'), "post", "application/json")
+
+        expect(@agent.events.last.payload['mapped_variables']).to eq({"tc" => "101 and more"})
+      end
+    end
+  end
+end
